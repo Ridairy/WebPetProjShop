05a6fb1f13a9a5094473f70cca6403b982e297ed
Ridariy <xdsanya@gmail.com>
2018-07-16 18:39:13 +0300

    Alert disable

--- WebPetProjShop/Scripts/AlertScripts.js
+++ WebPetProjShop/Scripts/AlertScripts.js
@@ -1,17 +1,17 @@
-﻿function DoAlert(type, msg) {
-    var randId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    var ConfirmType;
-    switch (type) {
-        case 'success': ConfirmType = type; break;
-        default: ConfirmType = undefined;
-    }
-    if (ConfirmType !== undefined) {
-        var html = '<div class="alert alert-' + ConfirmType + ' col-7 PopUpAlert" role="alert" id="' + randId + '">Some success text here lol</div>'
-        $('#message')
-            .append(html);
-        $("#" + randId).delay(2000).slideUp(500);
-    }
+﻿//function DoAlert(type, msg) {
+//    var randId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+//    var ConfirmType;
+//    switch (type) {
+//        case 'success': ConfirmType = type; break;
+//        default: ConfirmType = undefined;
+//    }
+//    if (ConfirmType !== undefined) {
+//        var html = '<div class="alert alert-' + ConfirmType + ' col-7 PopUpAlert" role="alert" id="' + randId + '">Some success text here lol</div>'
+//        $('#message')
+//            .append(html);
+//        $("#" + randId).delay(2000).slideUp(500);
+//    }
     
 
-};
+//};
 
